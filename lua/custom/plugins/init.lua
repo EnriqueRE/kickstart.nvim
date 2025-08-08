3928c44b9108269981ee2bc6ff060b59e7472269
Enrique Ramirez <enrique.ramirez35@gmail.com>
2025-08-08 16:53:30 -0600

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,27 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.opt.swapfile = false
+vim.opt.colorcolumn = "120"
+
+-- [[ Basic Keymaps ]]
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Go to explorer'}) -- opening netrwd
+
+-- moves selection, it even indents if it's inside an if statement or function, I know cool af
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- keeps cursor at the beginning of
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- page jumps but keeping the cursor at the center
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "Q", "<nop>")                                                          -- never use capital quotes
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)                                       -- format stuff
+vim.keymap.set("n", "<leader>srw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Search the whole file and replace the word/pattern'}) -- replace current word
+
+
 return {}
